2626bf237826e0e0c4e25531e5e6a35d1b10d114
wangyan666 <wyan16888@gmail.com>
2021-09-10 12:38:01 +0800

    项目调整目录

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="container">
+  <div id="app">
+    <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name:'App',
+  name: 'App'
 }
 </script>
 
-<style lang="less">
-
-</style>
+<style lang="less"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  
 ]
 
 const router = new VueRouter({
